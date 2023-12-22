6cc88b51878e8247963249b2b0ca6f3d7cde2e26
Dylan <git@applesandbananas.co.uk>
2023-12-22 14:55:20 +0000

    image rename

--- src/scenes/connect.py
+++ src/scenes/connect.py
@@ -5,7 +5,7 @@ from util import Image
 class ConnectScene():
     def __init__(self, screen: pygame.Surface) -> None:
         self.screen = screen
-        self.background = Image("scenes/connect/background.png")
+        self.background = Image("scenes/connect/screen.png")
 
     def render(self):
         self.background.render(self.screen)
