eff05b1895a903d95e543dfa2573759d61d5f1ce
Dylan <git@applesandbananas.co.uk>
2023-12-25 11:00:26 +0000

    run script

---
+++ src/run.py
@@ -0,0 +1,5 @@
+import os
+
+cmd = "xinit -e python3 ~/Wiiid/src/wiiid.py;"
+
+os.system(cmd*50)
--- src/util/restart.py
+++ src/util/restart.py
@@ -1,5 +1,4 @@
 import os, sys
 
 def restart():
-    print(sys.argv[0])
-    os.execv(sys.argv[0], sys.argv)
+    sys.exit()
