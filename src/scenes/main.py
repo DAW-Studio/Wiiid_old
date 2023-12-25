0818d6f525e336e82e10a7f6cb072e35b17aedda
Dylan <git@applesandbananas.co.uk>
2023-12-25 19:32:37 +0000

    font size

--- src/scenes/main.py
+++ src/scenes/main.py
@@ -7,8 +7,8 @@ class MainScene():
     def __init__(self, screen: pygame.Surface) -> None:
         self.screen = screen
         self.background = Image("interface.png", (0,0))
-        self.font = pygame.font.SysFont("Menlo",32)
-        self.logHeight = 24
+        self.font = pygame.font.SysFont("Menlo",36)
+        self.logHeight = self.font.get_height() + 10
         self.maxLogs = 12
         self.logs = []
         self.log("wiimote connected")
