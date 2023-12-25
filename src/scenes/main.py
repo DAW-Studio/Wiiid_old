4adf77539fad4c80789be164f9c4ba814c86dbd8
Dylan <git@applesandbananas.co.uk>
2023-12-25 19:25:27 +0000

    log vars resize max pos

--- src/scenes/main.py
+++ src/scenes/main.py
@@ -7,17 +7,20 @@ class MainScene():
     def __init__(self, screen: pygame.Surface) -> None:
         self.screen = screen
         self.background = Image("interface.png", (0,0))
-        self.font = pygame.font.SysFont("Menlo",24)
+        self.font = pygame.font.SysFont("Menlo",48)
+        self.logHeight = 26
+        self.maxLogs = 12
         self.logs = []
         self.log("wiimote connected")
 
     def log(self, *args):
         self.logs.append(", ".join([str(arg) for arg in args]))
+        if len(self.logs) > self.maxLogs:
+            self.logs.pop(0)
 
 
     def render(self):
         self.background.render(self.screen)
-        height = 18
         for i, message in enumerate(self.logs):
             y = len(self.logs)-i
-            self.screen.blit(self.font.render(message,1,(255,255,255)), (40,600-(height*y)))
+            self.screen.blit(self.font.render(message,1,(255,255,255)), (40,690-self.logHeight-(self.logHeight*y)))
